99decbe48869ac7c4fcec76ff11db9d7a4581d80
MargaretEarnest <molly7312000@gmail.com>
2020-07-04 15:17:11 -0400

    Start of PWA edits

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
+const { Pool } = require('pg');
+var connectionString = 'postgresql://postgres:wpivexu@localhost:5432/testdata';
+// const pool = new Pool({
+  // connectionString: process.env.DATABASE_URL,
+  // ssl: {
+    // rejectUnauthorized: true
+  // }
+// });
+const pool = new Pool({
+    connectionString: 'postgresql://postgres:wpivexu@localhost:5432/testdata',
+    ssl: process.env.DATABASE_URL ? true : false
+})
+
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
@@ -7,4 +20,17 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect();
+	  // const result = await client.query('SELECT current_database()');
+      const result = await client.query('SELECT * FROM test_table');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('pages/db', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
