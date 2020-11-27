450537ae8d9e570a31f290739516abfaaf7f2988
MargaretEarnest <molly7312000@gmail.com>
2020-11-27 17:02:54 -0500

    Got the connection between server and page working, doesn't store data yet

--- index.js
+++ index.js
@@ -18,34 +18,19 @@ const bodyParser = require("body-parser");
 const app = express();
 const PORT = process.env.PORT || 5000
 
-app.post("/storeMatch", bodyParser.json(), function(req, res) {
-  set('vex', req.body.matchVal).then(() => console.log('It worked!')).catch(err => console.log('It failed!', err));
-});
-
-app.post("/getMatch", bodyParser.json(), function(req, res) {
-  get(req.body.matchKey).then(
-	val => {
-		console.log(val);
-		res.json(val);
-	});
-});
-
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .get('/db', async (req, res) => {
-    try {
-      const client = await pool.connect();
-	  // const result = await client.query('SELECT current_database()');
-      const result = await client.query('SELECT * FROM test_table');
-      const results = { 'results': (result) ? result.rows : null};
-      res.render('pages/db', results );
-      client.release();
-    } catch (err) {
-      console.error(err);
-      res.send("Error " + err);
-    }
-  })
+  .post("/getMatch", bodyParser.json(), function(req, res) {
+	  idb.get(req.body.matchKey).then(
+		val => {
+			console.log(val);
+			res.json(val);
+		});
+	})
+  .post("/storeMatch", bodyParser.json(), function(req, res) {
+	  idb.set('vex', req.body.matchVal).then(() => console.log('It worked!')).catch(err => console.log('It failed!', err));
+	})
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
