1d969aebe00292d8504753a03c7c9bf686710a3d
MargaretEarnest <molly7312000@gmail.com>
2020-11-27 15:53:41 -0500

    Attempt at storing data locally

--- index.js
+++ index.js
@@ -1,19 +1,34 @@
-const { Pool } = require('pg');
-var connectionString = 'postgresql://postgres:wpivexu@localhost:5432/testdata';
+// const { Pool } = require('pg');
+// var connectionString = 'postgresql://postgres:wpivexu@localhost:5432/testdata';
+// // const pool = new Pool({
+  // // connectionString: process.env.DATABASE_URL,
+  // // ssl: {
+    // // rejectUnauthorized: true
+  // // }
+// // });
 // const pool = new Pool({
-  // connectionString: process.env.DATABASE_URL,
-  // ssl: {
-    // rejectUnauthorized: true
-  // }
-// });
-const pool = new Pool({
-    connectionString: 'postgresql://postgres:wpivexu@localhost:5432/testdata',
-    ssl: process.env.DATABASE_URL ? true : false
-})
+    // connectionString: 'postgresql://postgres:wpivexu@localhost:5432/testdata',
+    // ssl: process.env.DATABASE_URL ? true : false
+// })
 
 const express = require('express')
 const path = require('path')
+const idb = require('idb-keyval');
+const bodyParser = require("body-parser");
+const app = express();
 const PORT = process.env.PORT || 5000
+
+app.post("/storeMatch", bodyParser.json(), function(req, res) {
+  set('vex', req.body.matchVal).then(() => console.log('It worked!')).catch(err => console.log('It failed!', err));
+});
+
+app.post("/getMatch", bodyParser.json(), function(req, res) {
+  get(req.body.matchKey).then(
+	val => {
+		console.log(val);
+		res.json(val);
+	});
+});
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
